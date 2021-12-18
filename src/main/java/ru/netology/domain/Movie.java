87edec7a0b60900f1b2da8e1c63bbf5c9302b9b8
Elena Ryzhkina <silena003@gmail.com>
2021-12-18 15:02:22 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Movie {
+    public static void main (String[] args) {
+        String[] movie = {
+                "Бладшот",
+                "Вперед",
+                "Отель Белград",
+                "Джентльмены",
+                "Человек-невидимка",
+                "Тролли. Мировой тур",
+                "Номер один"
+        };
+
+    }
+}
---
+++ src/main/java/ru/netology/domain/NewManager.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class NewManager {
+    public class MovieItem {
+        private int id;
+        private int movieId;
+        private String movieName;
+    }
+
+}
+
